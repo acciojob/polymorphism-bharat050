fa81d2c14c055cf68c42e80152664507b4d82211
Bharat <aimhigh050@gmail.com>
2023-06-20 01:57:08 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,21 +9,20 @@ public class Main {
         System.out.println(p.product(5, 7, 8));
         System.out.println(p.product(3.4, 2.2));
     }
+    static class Product{
+        Product(){
 
-}
-class Product{
-    Product(){
-
-    }
-    public int product(int x, int y)
-    {
-        return x+y;
-    }
-    public int product(int x, int y, int z) {
-        return x+y+z;
-    }
-    public double product(double x, double y)
-    {
-        return x+y;
+        }
+        public int product(int x, int y)
+        {
+            return x+y;
+        }
+        public int product(int x, int y, int z) {
+            return x+y+z;
+        }
+        public double product(double x, double y)
+        {
+            return x+y;
+        }
     }
 }
